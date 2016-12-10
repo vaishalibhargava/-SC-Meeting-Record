e4d87c2ebeb0a8d7b1eae1915967d189672f7e39
vaishalibhargava <vaishali.bhargava@ssla.edu.in>
2016-12-10 20:56:25 +0530

    Side Navigation Bar
    A try at having a side navigation bar using Javascript. This will have
    an icon which when clicked will open into the list of different
    designations.

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  var x=
+  document.getElementById("mySidenav");
+  if(x.className === "sidenav") {
+    x.v=className += "responsive";
+  }
+  else {
+    x.className = "sidenav";
+  }
+}
