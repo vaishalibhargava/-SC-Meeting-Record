a1830f9b08c9eb07eb004652ca9f500731f5b934
vaishalibhargava <vaishali.bhargava@ssla.edu.in>
2016-12-10 23:50:02 +0530

    Some few changes.
    Tried to fix the navigation bar and make drop down list using
    Javascript.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function myFunction() {
   var x=
-  document.getElementById("mySidenav");
-  if(x.className === "sidenav") {
-    x.v=className += "responsive";
-  }
-  else {
-    x.className = "sidenav";
-  }
+  document.getElementById("myDropdown").classlist.toggle("show"););
 }
+window.onclick=function(e) {
+  if (!e.target.matches('.dropbtn')){
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length;d++){
+      var openDropDown = dropdowns[d];
+      if(openDropDown.classlist.contains('show')) {
+        openDropDown.classlist.remove('show');
+      }
+    }
+
+  }
