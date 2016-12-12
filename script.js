0a6149fdaa99de5806075e0292995f77f2b73c3d
vaishalibhargava <vaishali.bhargava@ssla.edu.in>
2016-12-12 21:20:58 +0530

    Login Form, Checkboxes Added
    A login form with user name, password and gender has been added and
    checkboxes in acoording to the designation has been added as well.

--- script.js
+++ script.js
@@ -1,66 +1,27 @@
-function myFunction() {
-  var x=
-  document.getElementById("myDropdown").classlist.toggle("show");
-}
-window.onclick=function(e) {
-  if (!e.target.matches('.dropbtn')){
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (var d = 0; d < dropdowns.length;d++){
-      var openDropDown = dropdowns[d];
-      if(openDropDown.classlist.contains('show')) {
-        openDropDown.classlist.remove('show');
-      }
-    }
-
-    function myFunction() {
-      document.getElementById("myDropdown").classList.toggle("show");
-    }
-    window.onclick=function(e) {
-      if (!e.target.matches('.dropbtn')){
-
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var d = 0; d < dropdowns.length; d++){
-          var openDropdown = dropdowns[d];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
-      }
-    }
-      </script>
 
     new Vue({
-      el: "#app",
+      el: '#app',
       data: {
         EventsDiscussion: [
 
-                ],
-                Discuss:""
-              },
-              methods: {
-                addDiscuss: function(){
-                  this.EventsDiscussion.push(this.Discuss);
-                  this.Discuss="";
-                },
-              removeDiscuss: function(){
-
-                this.legends.pop(this.legend);
-              }
+            ],
+            Discuss:"",
+            attend:0,
+            checkedNames: [],
+          },
+          methods: {
+            addDiscussion: function(){
+              this.EventsDiscussion.push(this.Discuss);
+              this.Discuss="";
+            },
+            removeDiscussion: function(){
+              this.EventsDiscussion.pop(this.Discuss);
+            },
+            Increase: function(){
+              this.attend +=1;
+            },
+            Decrease: function(){
+              this.attend -=1;
             }
-         })
-
-         new Vue({
-           el:'#app',
-           data: {
-             attend: 0
-           },
-           methods: {
-             Increase: function(){
-               this.attend +=1;
-             },
-             Decrease: function(){
-               this.attend -=1;
-             }
-           }
-         });
+        },
+     });
